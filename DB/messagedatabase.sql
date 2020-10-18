b53fab7c01dfc9dd6a98e3fa0bef69b05269a69d
Marco <gregorymarco99@gmail.com>
2020-10-18 01:16:27 -0400

    store the correct precision for lat/long

--- DB/messagedatabase.sql
+++ DB/messagedatabase.sql
@@ -2,8 +2,8 @@
 CREATE TABLE Messages (
     ID SERIAL NOT NULL,
     MessageContent varchar(1000),
-    Lat decimal(10),
-    Long decimal(10),
+    Lat decimal(11,8),
+    Long decimal(11,8),
     heure TIMESTAMP,
     PRIMARY KEY(ID)
 )
